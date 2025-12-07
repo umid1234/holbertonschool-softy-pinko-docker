7a77810ac0816182242c392b8691fff8478dc24e
Umud Abutalibov 17970 <uabutalibov1790@ada.edu.az>
2025-12-07 18:58:56 +0400

    final

--- task2/back-end/api.py
+++ task2/back-end/api.py
@@ -7,5 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5252)
-
+    # This keeps the server running and reachable from outside the container
+    app.run(host='0.0.0.0', port=5252, debug=False)
--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # This keeps the server running and reachable from outside the container
-    app.run(host='0.0.0.0', port=5252, debug=False)
+    app.run(host='0.0.0.0', port=5252)
